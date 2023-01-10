0044bd3315740c0aa863e1ace0e93b32b0230f48
Alexander <nicky-nico-1998@outlook.com>
2023-01-10 01:00:25 +0000

    Final Code

--- starter/script.js
+++ starter/script.js
@@ -88,64 +88,100 @@ var upperCasedCharacters = [
     'Z'
 ];
 
+var passwordLength;
+var options;
+
+
 // Function to prompt user for password options
 function getPasswordOptions() {
-    var specialOption;
-    if (confirm("Click OK to confirm including special characters.")) {
+    passwordLength = prompt("How many characters would you like your password to contain?");
+    console.log(passwordLength);
 
+    if (passwordLength < 10) {
+        alert("Password length must be at least 10 characters");
+    } else if (passwordLength > 64) {
+        alert("Password length must be at least 64 characters");
     } else {
-        alert("Cancel");
+        var specialOption = confirm("Click OK to confirm including special characters.");
+        // console.log("Special characters YES");
+        var numericOption = confirm("Click OK to confirm including numeric characters.");
+        // console.log("Numeric characters YES");
+        var lowerCaseOption = confirm("Click OK to confirm including lower case characters.");
+        // console.log("Lower case characters YES");
+        var upperCaseOption = confirm("Click OK to confirm including upper case characters.");
+        // console.log("Upper case characters YES");
     };
-
-    var numericOption;
-    if (confirm("Click OK to confirm including numeric characters.")) {
-        alert("OK")
-    } else {
-        alert("Cancel");
+    //if user select neither option 
+    if (!specialOption && !numericOption && !lowerCaseOption && !upperCaseOption) {
+        alert("You must choose a criteria");
+        // if user select all the options
+    } else if (specialOption && numericOption && lowerCaseOption && upperCaseOption) {
+        options = specialCharacters.concat(numericCharacters, lowerCasedCharacters, upperCasedCharacters);
+        console.log(options);
+        // if user select only 3 options
+    } else if (specialOption && numericOption && lowerCaseOption) {
+        options = options = specialCharacters.concat(numericCharacters, lowerCasedCharacters);
+        console.log(options);
+    } else if (specialOption && numericOption && upperCaseOption) {
+        options = specialCharacters.concat(numericCharacters, upperCasedCharacters);
+        console.log(options);
+    } else if (specialOption && lowerCaseOption && upperCaseOption) {
+        options = specialCharacters.concat(lowerCasedCharacters, upperCasedCharacters);
+        console.log(options);
+    } else if (numericOption && lowerCaseOption & upperCaseOption) {
+        options = numericCharacters.concat(lowerCasedCharacters, upperCasedCharacters);
+        console.log(options);
+        // if user select only 2 options
+    } else if (specialOption && numericOption) {
+        options = specialCharacters.concat(numericCharacters);
+        console.log(options);
+    } else if (specialOption && lowerCaseOption) {
+        options = specialCharacters.concat(lowerCasedCharacters);
+    } else if (specialOption && upperCaseOption) {
+        options = specialCharacters.concat(upperCasedCharacters);
+    } else if (numericOption && lowerCaseOption) {
+        options = numericCharacters.concat(lowerCasedCharacters);
+    } else if (numericOption && upperCaseOption) {
+        options = numericCharacters.concat(upperCasedCharacters);
+    } else if (lowerCaseOption && upperCaseOption) {
+        options = lowerCasedCharacters.concat(upperCasedCharacters);
+        // if user select only 1 option
+    } else if (specialOption) {
+        options = specialCharacters;
+    } else if (numericOption) {
+        options = numericCharacters;
+    } else if (lowerCaseOption) {
+        options = lowerCasedCharacters;
+    } else if (upperCaseOption) {
+        options = upperCasedCharacters;
     };
-
-    var lowerCaseOption;
-    if (confirm("Click OK to confirm including lower case characters.")) {
-        alert("OK")
-    } else {
-        alert("Cancel");
-    };
-
-    var uppercaseOption;
-    if (confirm("Click OK to confirm including upper case characters.")) {
-        alert("OK")
-    } else {
-        alert("Cancel");
-    };
+    getRandom();
 };
 
 
 // Function for getting a random element from an array
 function getRandom(arr) {
+    var storePassword = [];
 
-    for (var i = 0; i < arr.length; i++) {
-        var randomSpecial = specialCharacters[Math.floor(Math.random() * specialCharacters.length)];
-        console.log(randomSpecial);
-        var randomNumeric = numericCharacters[Math.floor(Math.random() * numericCharacters.length)];
-        console.log(randomNumeric);
-        var randomLowerCased = lowerCasedCharacters[Math.floor(Math.random() * lowerCasedCharacters.length)];
-        console.log(randomLowerCased);
-        var randomUpperCased = upperCasedCharacters[Math.floor(Math.random() * upperCasedCharacters.length)];
-        console.log(randomUpperCased);
-    }
+    for (var i = 0; i < passwordLength; i++) {
+        // var randomSpecial = specialCharacters[Math.floor(Math.random() * specialCharacters.length)];
+        // var randomNumeric = numericCharacters[Math.floor(Math.random() * numericCharacters.length)];
+        // var randomLowerCased = lowerCasedCharacters[Math.floor(Math.random() * lowerCasedCharacters.length)];
+        // var randomUpperCased = upperCasedCharacters[Math.floor(Math.random() * upperCasedCharacters.length)];
+        var allOptions = options[Math.floor(Math.random() * options.length)];
+        storePassword.push(allOptions);
+    };
+    var generatedPassword = storePassword.join("");
+    console.log("The password is " + storePassword);
+
+    return generatedPassword;
 };
+
 
 // Function to generate password with user input
 function generatePassword() {
-    var lengthOption = prompt("How many characters would you like your password to contain?");
-    if (lengthOption <= 10) {
-        alert("Password length must be at least 10 characters");
-    } else if (lengthOption >= 64) {
-        alert("Password length must be at least 10 characters");
-    } else {
-        getPasswordOptions();
-    }
-    // return getRandom();
+    getPasswordOptions();
+    return getRandom();
 };
 
 // Get references to the #generate element
